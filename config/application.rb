e7a0b2ee2efa82a453d5bc959fc79c69bd05040d
Mauro Almiron <mdavid.almiron@gmail.com>
2022-11-28 20:49:51 -0300

    add local_env config

--- config/application.rb
+++ config/application.rb
@@ -17,5 +17,12 @@ module Fideapp
     # the framework and any gems in your application.
     config.time_zone = "America/Argentina/Buenos_Aires"
     config.i18n.default_locale = :es
+
+    config.before_configuration do
+      env_file = File.join(Rails.root, 'config', 'local_env.yml')
+      YAML.load(File.open(env_file)).each do |key, value|
+        ENV[key.to_s] = value
+      end if File.exists?(env_file)
+    end
   end
 end
