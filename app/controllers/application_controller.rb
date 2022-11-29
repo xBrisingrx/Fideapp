f2f7eb6cbd9b8b0fce789197eac4376982724353
Mauro Almiron <mdavid.almiron@gmail.com>
2022-11-28 21:07:09 -0300

    get lang from header request

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,13 @@
 class ApplicationController < ActionController::Base
+	around_action :switch_locale
+
+  def switch_locale(&action)
+    I18n.with_locale(locale_from_header, &action)
+  end
+
+  private
+
+  def locale_from_header # obtenemos el lenguaje desde la cabecera de la peticion
+    request.env['HTTP_ACCEPT_LANGUAGE'].scan(/^[a-z]{2}/).first
+  end
 end
