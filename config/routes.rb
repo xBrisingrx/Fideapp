2d309c1b44bfd986f4d6d0fc66c74300ab0f0ac2
Mauro Almiron <mdavid.almiron@gmail.com>
2022-11-28 21:59:14 -0300

    changes in user namespace
    with path: '' remove authenticate in web url
    with as: '' remove authenticate in route helper

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   root 'main#welcome'
   get 'main/welcome'
 
-  namespace :authentication do
+  namespace :authentication, path: '', as: '' do
     resource :users, only: [:index,:new, :create]
   end
 end
