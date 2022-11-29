9a1dd43a9ec4351d212a4802ca782c03a54bb466
Mauro Almiron <mdavid.almiron@gmail.com>
2022-11-28 21:58:05 -0300

    add user index route

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,6 @@ Rails.application.routes.draw do
   get 'main/welcome'
 
   namespace :authentication do
-    resource :users, only: [:index, :new, :create]
+    resource :users, only: [:index,:new, :create]
   end
 end
