b71acceff12d3091ca387adb6c71fbed6e5c748b
Brisingr <mdavid.almiron@gmail.com>
2022-11-29 16:46:59 -0300

    start feature session

--- config/routes.rb
+++ config/routes.rb
@@ -4,5 +4,6 @@ Rails.application.routes.draw do
 
   namespace :authentication, path: '', as: '' do
     resources :users, only: [:index,:new, :create]
+    resources :sessions, only: [:new, :create]
   end
 end
