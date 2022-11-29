ee89dba17326a0e145df7dbb03a2325c9fb64b01
Mauro Almiron <mdavid.almiron@gmail.com>
2022-11-29 20:46:35 -0300

    clean session controller

--- app/controllers/authentication/sessions_controller.rb
+++ app/controllers/authentication/sessions_controller.rb
@@ -1,58 +1,21 @@
 class Authentication::SessiosController < ApplicationController
-	before_action :set_session, only: %i[ show edit update destroy ]
-
-  def index
-    @users = User.all
-  end
-
-  def show
-  end
-
+	skip_before_action :no_login
   def new
-    @user = User.new
-  end
 
-  def edit
   end
 
   def create
-    @user = User.new(user_params)
-
-    respond_to do |format|
-      if @user.save
-        format.json { render :show, status: :created, location: @user }
-      else
-        format.json { render json: @user.errors, status: :unprocessable_entity }
-      end
+    if user&.authenticate(params[:password])
+      session[:user_id] = user.id
+      redirect_to root_url, notice: "Sessión iniciada!"
+    else
+      flash.now[:alert] = "Nombre de usuario o contraseña invalido"
+      render "new"
     end
-  end
-
-  def update
-    respond_to do |format|
-      if @user.update(user_params)
-        # format.html { redirect_to @user, notice: "User was successfully updated." }
-        format.json { render :show, status: :ok, location: @user }
-      else
-        # format.html { render :edit, status: :unprocessable_entity }
-        format.json { render json: @user.errors, status: :unprocessable_entity }
-      end
-    end
-  end
+  end  
 
   def destroy
-    @user.destroy
-    respond_to do |format|
-      format.html { redirect_to users_url, notice: "User was successfully destroyed." }
-      format.json { head :no_content }
-    end
+    session[:user_id] = nil
+    redirect_to root_url, notice: "Logged out!"
   end
-
-  private
-    def set_session
-      @user = User.find(params[:id])
-    end
-
-    def user_params
-      params.require(:user).permit(:name, :username, :email,:password, :password_confirmation, :rol)
-    end
 end
