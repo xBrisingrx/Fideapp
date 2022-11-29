f4b50cd8bce41e182c6c19f70f0de556186322b9
Mauro Almiron <mdavid.almiron@gmail.com>
2022-11-28 21:50:19 -0300

    applying unify template

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -1,15 +1,55 @@
-// This is a manifest file that'll be compiled into application.js, which will include all the files
-// listed below.
-//
-// Any JavaScript/Coffee file within this directory, lib/assets/javascripts, or any plugin's
-// vendor/assets/javascripts directory can be referenced here using a relative path.
-//
-// It's not advisable to add code directly here, but if you do, it'll appear at the bottom of the
-// compiled file. JavaScript code in this file should be added after the last require_* statement.
-//
-// Read Sprockets README (https://github.com/rails/sprockets#sprockets-directives) for details
-// about supported directives.
-//
 //= require rails-ujs
 //= require activestorage
-//= require_tree .
+//= require activestorage
+//= require unify/jquery.min
+//= require unify/jquery-migrate.min
+//= require unify/popper.min
+//= require unify/bootstrap.min
+//= require unify/jquery.cookie 
+//= require unify/appear
+//= require unify/jquery.mCustomScrollbar.concat.min
+//= require unify/chartist.min
+//= require unify/chartist-plugin-tooltip
+//= require unify/hs.core
+//= require unify/hs.side-nav
+//= require unify/hs.hamburgers
+//= require unify/hs.dropdown
+//= require unify/hs.scrollbar
+//= require unify/hs.area-chart
+//= require unify/hs.donut-chart
+//= require unify/hs.bar-chart
+//= require unify/hs.toastr
+//= require unify/hs.popup
+//= require unify/noty.min
+
+// require_tree .
+
+
+$(document).on('ready', function () {
+  // initialization of hamburger
+  $.HSCore.helpers.HSHamburgers.init('.hamburger');
+
+  // initialization of sidebar navigation component
+  $.HSCore.components.HSSideNav.init('.js-side-nav', {
+    afterOpen: function() {
+      setTimeout(function() {
+        $.HSCore.components.HSAreaChart.init('.js-area-chart');
+        $.HSCore.components.HSDonutChart.init('.js-donut-chart');
+        $.HSCore.components.HSBarChart.init('.js-bar-chart');
+      }, 400);
+    },
+    afterClose: function() {
+      setTimeout(function() {
+        $.HSCore.components.HSAreaChart.init('.js-area-chart');
+        $.HSCore.components.HSDonutChart.init('.js-donut-chart');
+        $.HSCore.components.HSBarChart.init('.js-bar-chart');
+      }, 400);
+    }
+  });
+
+  // initialization of HSDropdown component
+  $.HSCore.components.HSDropdown.init($('[data-dropdown-target]'), {dropdownHideOnScroll: false})
+
+  // initialization of custom scrollbar
+  $.HSCore.components.HSScrollBar.init($('.js-custom-scroll'))
+})
