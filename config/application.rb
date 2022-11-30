d615b61f2fcecaf88da9820fa8ced29c10974aca
Brisingr <mdavid.almiron@gmail.com>
2022-11-30 13:28:35 -0300

    remove: coffee script

--- config/application.rb
+++ config/application.rb
@@ -11,10 +11,13 @@ module Fideapp
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.2
 
+    config.generators.javascript_engine = :js
+    
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
     # the framework and any gems in your application.
+
     config.time_zone = "America/Argentina/Buenos_Aires"
     config.i18n.available_locales = [:en, :es]
     config.i18n.default_locale = :es
