a43214a9c7ba9a950de9c68df7d61a6138500daa
Brisingr <mdavid.almiron@gmail.com>
2022-12-01 13:12:55 -0300

    add: helpers for format date and money

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,13 @@
 module ApplicationHelper
+	def format_currency(num)
+		number_to_currency(num, {:unit => '', :separator => ',', :delimiter => '.', :precision => 2})
+	end
+
+	def active_class(link_path)
+	    current_page?(link_path) ? "active" : ""
+	end
+
+	def date_format date
+		date.strftime('%d-%m-%y')
+	end
 end
