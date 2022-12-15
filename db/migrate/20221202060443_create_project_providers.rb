d3529b76681ee35a2c0013ca5b8480e7e4cfcaea
Mauro Almiron <mdavid.almiron@gmail.com>
2022-12-14 22:20:11 -0300

    add: value iva and type total
    type_total is the value in which calculate the total price

--- db/migrate/20221202060443_create_project_providers.rb
+++ db/migrate/20221202060443_create_project_providers.rb
@@ -5,8 +5,10 @@ class CreateProjectProviders < ActiveRecord::Migration[5.2]
       t.references :provider, foreign_key: true
       t.references :payment_method, foreign_key: true
       t.references :provider_role, foreign_key: true
+      t.string :type_total, comment: 'El proveedor puede calcular su precio por el subtotal de la obra o por el precio valor inicial de la obra'
       t.decimal :porcent,precision: 15, scale: 2, default: 0.0, comment: '% que cobra'
       t.decimal :iva,precision: 15, scale: 2, default: 0.0 
+      t.decimal :value_iva,precision: 15, scale: 2, default: 0.0, comment: "El valor en pesos del IVA"
       t.decimal :price,precision: 15, scale: 2, default: 0.0, comment: 'Precio que cobra'
       t.decimal :price_calculate,precision: 15, scale: 2, default: 0.0, comment: 'Precio calculado cuando el proveedor cobra por porcentaje de obra'
       t.boolean :active, default: true
