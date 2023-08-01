d49e11c72f77a87b41f754718bc666f7342e12fd
Mauro Almiron <mdavid.almiron@gmail.com>
2023-08-01 20:43:14 -0300

    set custom error page

--- config/application.rb
+++ config/application.rb
@@ -17,7 +17,7 @@ module Fideapp
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
     # the framework and any gems in your application.
-
+    config.exceptions_app = self.routes
     config.time_zone = "America/Argentina/Buenos_Aires"
     config.i18n.available_locales = [:en, :es]
     config.i18n.default_locale = :es
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
+  
+  match "/404", via: :all, to: "errors#not_found"
+  match "/500", via: :all, to: "errors#internal_server_error"
+  
   resources :refinancied_sales, only: [:index, :new, :create]
   resources :payments
   resources :credit_notes
