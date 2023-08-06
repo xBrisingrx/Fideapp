5b4f55ebfe88ee047a70376e204aa3b5abc48952
Mauro Almiron <mdavid.almiron@gmail.com>
2023-08-06 19:49:01 -0300

    add validates

--- app/models/adjust.rb
+++ app/models/adjust.rb
@@ -15,9 +15,16 @@
 #
 class Adjust < ApplicationRecord
   belongs_to :fee
+  # Ligamos al payment solo cuando se genera al pagar una cuota
+  # con eso ganamos que al eliminar un pago se elimina esta mora 
+  belongs_to :payment, optional: true
 
   after_create :update_total_value_fee 
 
+  validates :value, 
+    presence: true,
+    numericality: { greater_than: 0 }
+    
   private
 
   def update_total_value_fee
--- app/models/interest.rb
+++ app/models/interest.rb
@@ -16,4 +16,11 @@
 class Interest < ApplicationRecord
   belongs_to :fee_payment, optional: true
   belongs_to :fee
+  # Ligamos al payment solo cuando se genera al pagar una cuota
+  # con eso ganamos que al eliminar un pago se elimina esta mora 
+  belongs_to :payment, optional: true 
+
+  validates :value, 
+    presence: true,
+    numericality: { greater_than: 0 }
 end
