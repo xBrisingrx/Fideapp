d4971d8f38b78c5edd0135dccabbac74b879925c
Mauro Almiron <mdavid.almiron@gmail.com>
2023-08-07 09:23:57 -0300

    remove default value

--- db/migrate/20230806192828_change_payment_to_payment.rb
+++ db/migrate/20230806192828_change_payment_to_payment.rb
@@ -1,5 +1,5 @@
 class ChangePaymentToPayment < ActiveRecord::Migration[5.2]
   def change
-    change_column :payments, :payment, :decimal,precision: 15, scale: 2, default: 0.5
+    change_column :payments, :payment, :decimal,precision: 15, scale: 2
   end
 end
